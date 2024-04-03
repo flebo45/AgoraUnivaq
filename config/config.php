6ef41b9f567eeda7f2e6e6711ad074cc866030a8
flebo45 <alessio.torrieri@gmail.com>
2024-04-03 12:56:38 +0200

    Added agora.sql(for feature implementations), added Installation.php for autocreating the db without cli

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'provaFinale');
+define('DB_NAME', 'provaTesi');
 define('DB_USER', 'root');
 define('DB_PASS', 'pippo');
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once "autoload.php";
 require_once "StartSmarty.php";
+require_once "Installation.php";
 
+Installation::install();
 $fc = new CFrontController();
 $fc->run($_SERVER['REQUEST_URI']);
